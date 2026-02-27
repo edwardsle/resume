b84835754f326aa06c588da932139718904afa71
Ed Le <36517244+edwardsle@users.noreply.github.com>
2026-02-26 18:13:17 -0800

    New design

--- app/resume/jobtag.tsx
+++ app/resume/jobtag.tsx
@@ -1,10 +1,11 @@
 interface JobTagProps {
   text: string;
 }
+
 export default function JobTag({ text }: JobTagProps) {
   return (
-    <span className="inline-block bg-blue-100 px-3 py-1 rounded-full">
-      { text }
+    <span className="inline-block text-xs px-3 py-1 rounded-full bg-neutral-100 text-neutral-600 transition-all duration-200 hover:bg-neutral-900 hover:text-white cursor-default">
+      {text}
     </span>
   );
 }
