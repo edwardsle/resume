341e5d995edaa07cadb3ee02a6f2359097852f44
Ed Le <36517244+edwardsle@users.noreply.github.com>
2026-02-26 18:22:10 -0800

    New Design

--- app/resume/projects/orinstream.tsx
+++ app/resume/projects/orinstream.tsx
@@ -1,7 +1,7 @@
 export default function OrinStream() {
   return (
     <div className="text-gray-800">
-      <h1 className="">OrinSteam</h1>
+      <h1 className="">Sample</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur ea
         nobis praesentium deleniti dignissimos pariatur dicta! Nisi minus iure
